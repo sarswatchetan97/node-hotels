5d5008ee47918c5a7afe9b3a4f37a5e184c088a2
Chetan Sarswat <sarswatchetan97@gmail.com>
2025-07-12 18:59:51 +0530

    Shift mongodb local to mongodb atlas cluster

--- server.js
+++ server.js
@@ -13,9 +13,11 @@ console.log(typeof jsonStringified)*/
 const express = require('express');
 const app = express();
 const db = require('./db');
+require('dotenv').config();
 
 const bodyParser = require('body-parser')
 app.use(bodyParser.json());
+const PORT = process.env.PORT || 3000;
 
 //const Person = require('./models/Person')
 const MenuItem = require('./models/MenuItem');
@@ -126,6 +128,7 @@ const menuItemRoutes = require('./routes/menuItemRoutes');
 app.use('/person', personRoutes);
 app.use('/menu', menuItemRoutes);
 
-app.listen(3000, () => {
+
+app.listen(PORT, () => {
   console.log('Server is listiening on 3000');
 });
